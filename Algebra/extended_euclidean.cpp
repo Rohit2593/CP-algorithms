92b7a1dc0bc41161643974901851aea6549a7c3c
Rohit Agrawal <81074642+Rohit2593@users.noreply.github.com>
2022-12-11 12:01:13 +0530

    added comments

--- Algebra/extended_euclidean.cpp
+++ Algebra/extended_euclidean.cpp
@@ -1,3 +1,9 @@
+// let g be the gcd of a and b 
+// after performing the extended euclidean algorithm, we will get the values of x and y such that a.x + b.y = g 
+// if g == 1, then 
+// -> (a.x) mod y = 1 mod y    ( ie. modular inverse of a modulo y is x)
+// -> (b.y) mod x = 1 mod x 
+
 ll extended_euclidean(ll a, ll b, ll &x, ll &y) {
     if(b > a) 
         return extended_euclidean(b, a, y, x) ;
